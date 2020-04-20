e7d6e9e08a41284ac02ea7a435a3528445900665
Taylor Southorn <southort@uni.coventry.ac.uk>
2020-04-20 13:02:56 +0100

    Updates

---
+++ precache-manifest.40a440acc6aae144c0747d835e675cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771ed48271756bb96ae85e8af804cda0",
+    "url": "/Messenger_Project_303COM/index.html"
+  },
+  {
+    "revision": "15b2fffd5393a50dbd0e",
+    "url": "/Messenger_Project_303COM/static/css/main.60baa224.chunk.css"
+  },
+  {
+    "revision": "539947ca0c3127c31878",
+    "url": "/Messenger_Project_303COM/static/js/2.358248a2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Messenger_Project_303COM/static/js/2.358248a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b2fffd5393a50dbd0e",
+    "url": "/Messenger_Project_303COM/static/js/main.d3aca92a.chunk.js"
+  },
+  {
+    "revision": "e910835a6cb16395cd95",
+    "url": "/Messenger_Project_303COM/static/js/runtime-main.4f001da4.js"
+  }
+]);
--- src/databaseSetup.js
+++
@@ -1,3 +0,0 @@
-// const MongoClient = require('mongodb').MongoClient;
-// const bcrypt = require('bcrypt');
-// const saltRounds = 10;
