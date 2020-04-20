604b6ea258757b7f8eac8d7388f2e87bb5339da4
Taylor Southorn <southort@uni.coventry.ac.uk>
2020-04-20 19:46:28 +0100

    server setup

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -172,7 +172,7 @@ class ChatScreen extends React.Component {
                 <div className="chatContainer">
                     <div className="whosOnlineListContainer">
                         <OnlineUsers currentRoom={this.state.currentRoom} currentUser={this.state.currentUser} users={this.state.currentRoom.users}/>
-                        <RoomList subscribeToRoom={this.subscribeToRoom} rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]}/>
+                        <RoomList subscribeToRoom={this.subscribeToRoom} rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]} onScroll={this.onScroll}/>
                     </div>
                     <div className="chatListContainer">
                         <MessageList messages={this.state.messages} scrolled={this.state.scrolled} onScroll={this.onScroll} currentUser={this.state.currentUser}/>
